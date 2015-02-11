a490437201a20c6cabc89e7eda09d9c59e05a25e
sleekmason <sleekmason@gmail.com>
2015-02-11 01:07:57 -0600

    msm:vidc: fix wrong variable used in venc_internal.c

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 9 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 SMP PREEMPT Sun Feb 1 09:55:39 CST 2015"
+#define UTS_VERSION "#9 SMP PREEMPT Sun Feb 1 23:44:07 CST 2015"
 #define LINUX_COMPILE_BY "sleek"
 #define LINUX_COMPILE_HOST "sleekai"
 #define LINUX_COMPILER "gcc version 4.9.3 20141205 (prerelease) (crosstool-NG linaro-1.13.1+bzr2710 - Linaro GCC 2014.12 - Cortex-A15) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0SleekCyan2+"
+#define UTS_RELEASE "3.4.0SleekCyan3+"
