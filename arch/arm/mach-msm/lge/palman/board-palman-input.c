7a63ebf1abdf363dbc988728e5030fdf34d6a751
sleekmason <sleekmason@gmail.com>
2015-02-21 12:42:19 -0600

    added fastcharge

--- arch/arm/mach-msm/lge/palman/board-palman-input.c
+++ arch/arm/mach-msm/lge/palman/board-palman-input.c
@@ -581,7 +581,7 @@ static struct mxt_platform_data touch_mxt1188S_pdata = {
 	.ghost_detection_enable = 0,
 #endif
 #ifdef CONFIG_TOUCHSCREEN_ATMEL_KNOCK_ON
-	.knock_on_enable = true,
+	.knock_on_enable = false,
 #endif
 	.smart_cover_enable = true,
 };
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0SleekCyan4+"
+#define UTS_RELEASE "3.4.0SleekCyan5+"
